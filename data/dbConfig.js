7dbde81b175d44fa320e57ae81b6b3448dc18dc9
Jordan Marsaw <marsaw@energyhub.net>
2020-02-12 06:08:23 -0500

    ughhhh

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
-const environ = "production" || process.env.ENVIRON;
+const environ = "production";
 const db = knex(knexConfig[environ]);
 
 module.exports = db;
