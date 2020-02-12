8f9ea36f3a5f81cdadfb170c13084a8b6b8381d2
Jordan Marsaw <marsaw@energyhub.net>
2020-02-12 03:29:00 -0500

    functional wormholes working w/o encryption + pw

--- data/migrations/20200212013024_Wormhole.js
+++ data/migrations/20200212013024_Wormhole.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('Wormholes', tbl => {
     tbl.increments();
     tbl.boolean('tapped').defaultTo(false);
-    tbl.integer('wormhole_id').defaultTo(Math.ceil(Math.random() * 10000000));
+    tbl.integer('wormhole_id').defaultTo(0);
     tbl.string('content').notNullable(); 
   });  
 };
--- routes/wormhole/index.js
+++ routes/wormhole/index.js
@@ -20,28 +20,59 @@ router.get('/', async (req, res) => {
 
 router.post('/send', async (req, res) => {
   const { content } = req.body;
-  
+  let wormhole_id = Math.ceil(Math.random() * 9999999999);
+
   try {
-    if(!req.body.content) {
-      return res.status(409).json({ message: `You can't open an empty wormhole. (add content flag or content) ` });
+    if(!content) {
+      return res.status(409).json({ message: `You cannot open a wormhole without providing content.` });
     }
 
     const [ id ] = await model.add('Wormholes', { content });
-    if(id) {
-      const wormhole = await model.findBy('Wormholes', { id });
-      if(!wormhole) {
-        return res.status(404).json({ message: `There was an issue retrieving the wormhole!` });
-      }
-      return res.status(201).json({ 
-        message: `Wormhole successfully created! Send the user your Wormhole ID: ${wormhole.wormhole_id}` 
-      });
+    if(!id) {
+      return res.status(404).json({ message: `There was an issue adding Wormhole to the relay server.` });
+    }  
+    
+    let wormhole = await model.findBy('Wormholes', { wormhole_id });
+    if(wormhole) {
+      wormhole_id = Math.ceil(Math.random() * 9999999999);
     }
-  } catch({ message }) {
 
+    const wormholeIdUpdated = await model.update('Wormholes', id, { wormhole_id });
+    if(wormholeIdUpdated) {
+      wormhole = await model.findBy('Wormholes', { wormhole_id }); 
+    }
+
+    return res.status(201).json({ message: `Your wormhole has been opened! Share your ID with your intended recipient: ${ wormhole_id }.` });
+  } catch({ message }) {
+    return res.status(500).json({ message });
   }
 });
 
 router.post('/receive', async (req, res) => {
-  const { id } = req.body;
+  const { wormhole_id } = req.body;
+  
+  try {
+    if(!wormhole_id) {
+      return res.status(409).json({ message: `You must enter a wormhole ID.` });
+    }
+
+    const wormholeFound = await model.findBy('Wormholes', { wormhole_id });
+    if(!wormholeFound) {
+      return res.status(404).json({ message: `There was no wormhole found at the specified ID.` });
+    }
+    
+    const wormholeDeleted = await model.remove('Wormholes', wormholeFound.id);
+    if(!wormholeDeleted) {
+      return res.status(404).json({ message: `There was an issue closing the wormhole!` });
+    } 
+    
+    return res.status(200).json({ 
+      message: `Wormhole successfully retrieved and closed!`,
+      content: wormholeFound.content
+    });
+  } catch({ message }) {
+    return res.status(500).json({ message });
+  }
 });
+
 module.exports = router;
